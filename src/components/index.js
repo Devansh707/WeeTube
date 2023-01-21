2664300b0a7e198091e024562bbc90c8e911b7f5
Devansh707 <devanshsrivastava707@gmail.com>
2023-01-21 12:49:01 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+// this file is used for exporting all the components to app.js so as to make the code easily understandable
+export {default as Navbar} from './Navbar';
+export {default as Feed} from './Feed';
+export {default as ChannelDetail} from './ChannelDetail';
+export {default as VideoDetail} from './VideoDetail';
+export {default as SearchFeed} from './SearchFeed';
+export {default as SearchBar} from './SearchBar';
+export {default as Sidebar} from './Sidebar';
+export {default as Videos} from './Videos';
