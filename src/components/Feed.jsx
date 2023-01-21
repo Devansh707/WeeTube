8c658dc31152c890225f9869f389bc5b53503ea7
Devansh707 <devanshsrivastava707@gmail.com>
2023-01-21 15:16:42 +0530

    .gitignore is now working

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,13 +1,26 @@
 import {useState, useEffect} from 'react'
 import {Box, Stack, Typography} from '@mui/material';
+
+import { fetchFromApi } from '../utils/fetchFromApi';
+
 import { Sidebar, Videos} from './';
 
 
+
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${selectedCategory}`)
+        .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: {sx: "column", md:"row"} }}>
       <Box sx = {{ height: {sm: 'auto', md:'92vh'}, borderRight: '1px solid #3d3d3d', px: { sx:0, md: 2} }}>
-      <Sidebar />
+      <Sidebar selectedCategory= { selectedCategory } setSelectedCategory= { setSelectedCategory } />
       <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff'}}>
         Copyright 2023 @Devansh
       </Typography>
@@ -15,10 +28,10 @@ const Feed = () => {
 
       <Box p={2} sx={{ overflowY : 'auto', height: '90vh', flex: 2 }}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }} >
-          New <span style={{ color:'#F31403'}}>videos</span>
+          {selectedCategory} <span style={{ color:'#F31403'}}>videos</span>
         </Typography>
 
-        <Videos videos = {[]} />
+        <Videos videos = {videos} />
 
       </Box>
     </Stack>
--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -1,14 +1,21 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-    url: BASE_URL,
     params: {
-        // maxResult: 50
+        maxResult: 50,
     },
     headers: {
       'X-RapidAPI-Key': process.env.REACT_RAPID_API_KEY,
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
+
+
+//   This is the function that allows us to call the api's , pass some dynamic url and then call it from within any component in our application
+  export const fetchFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+    return data;
+  }
